6ec5a79ed6a462b52a538b35422ab4e7188c4985
Oleksii <shcherbaalex77@gmail.com>
2024-05-19 16:41:15 +0200

    added navigation components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
 import "./App.css";
 import "modern-normalize";
 
+import Navigation from "./components/Navigation/Navigation";
+import { Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage/MoviesPage"));
+const MovieDetailsPage = lazy(() =>
+  import("./pages/MovieDetailsPage/MovieDetailsPage")
+);
+const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
+
+const MovieCast = lazy(() => import("./components/MovieCast/MovieCast"));
+const MovieReviews = lazy(() =>
+  import("./components/MovieReviews/MovieReviews")
+);
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navigation />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<MovieCast />} />
+            <Route path="reviews" element={<MovieReviews />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
